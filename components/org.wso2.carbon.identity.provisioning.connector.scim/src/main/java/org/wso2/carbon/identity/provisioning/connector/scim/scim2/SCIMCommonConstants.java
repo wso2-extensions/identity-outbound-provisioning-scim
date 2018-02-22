056d4d8da7b16c4592096c08f5ac17d50fe01c77
Isuranga Perera <isurangamperera@gmail.com>
2018-02-22 23:51:38 +0530

    implement SCIM 2.0 outbound connector

---
+++ components/org.wso2.carbon.identity.provisioning.connector.scim/src/main/java/org/wso2/carbon/identity/provisioning/connector/scim/scim2/SCIMCommonConstants.java
@@ -0,0 +1,12 @@
+package org.wso2.carbon.identity.provisioning.connector.scim.scim2;
+
+public class SCIMCommonConstants {
+
+    public static final String DEFAULT = "default";
+
+    public static final int USER = 1;
+    public static final int GROUP = 2;
+
+    public static final String SCIM_CORE_CLAIM_DIALECT = "urn:ietf:params:scim:schemas:core:2.0";
+    public static final String SCIM_USER_CLAIM_DIALECT = "urn:ietf:params:scim:schemas:core:2.0:User";
+}
