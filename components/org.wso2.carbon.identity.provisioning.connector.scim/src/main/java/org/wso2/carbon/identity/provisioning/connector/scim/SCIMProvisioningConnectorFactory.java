0b73bd424f5b70d9aa4257522915462dc97ff05d
emsw.bandara <emsw.bandara@gmail.com>
2019-11-26 10:42:29 +0530

    Add configuration properties required for REST APIs

--- components/org.wso2.carbon.identity.provisioning.connector.scim/src/main/java/org/wso2/carbon/identity/provisioning/connector/scim/SCIMProvisioningConnectorFactory.java
+++ components/org.wso2.carbon.identity.provisioning.connector.scim/src/main/java/org/wso2/carbon/identity/provisioning/connector/scim/SCIMProvisioningConnectorFactory.java
@@ -21,8 +21,12 @@ package org.wso2.carbon.identity.provisioning.connector.scim;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.common.model.Property;
+import org.wso2.carbon.identity.application.common.model.SubProperty;
 import org.wso2.carbon.identity.provisioning.AbstractProvisioningConnectorFactory;
 import org.wso2.carbon.identity.provisioning.IdentityProvisioningException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author
@@ -52,4 +56,70 @@ public class SCIMProvisioningConnectorFactory extends AbstractProvisioningConnec
         return SCIM;
     }
 
+    /**
+     * Get Configuration Properties.
+     */
+    @Override
+    public List<Property> getConfigurationProperties() {
+
+        List<Property> configProperties = new ArrayList<>();
+        Property username = new Property();
+        username.setName(SCIMProvisioningConnectorConstants.SCIM_USERNAME);
+        username.setDisplayName("Username");
+        username.setRequired(true);
+        username.setType("string");
+        username.setDisplayOrder(1);
+        configProperties.add(username);
+
+        Property password = new Property();
+        password.setName(SCIMProvisioningConnectorConstants.SCIM_PASSWORD);
+        password.setDisplayName("Password");
+        password.setRequired(true);
+        password.setType("string");
+        password.setDisplayOrder(2);
+        configProperties.add(password);
+
+        Property userEp = new Property();
+        userEp.setName(SCIMProvisioningConnectorConstants.SCIM_USER_EP);
+        userEp.setDisplayName("User Endpoint");
+        userEp.setRequired(true);
+        userEp.setType("string");
+        userEp.setDisplayOrder(3);
+        configProperties.add(userEp);
+
+        Property groupEp = new Property();
+        groupEp.setName(SCIMProvisioningConnectorConstants.SCIM_GROUP_EP);
+        groupEp.setDisplayName("Group Endpoint");
+        groupEp.setRequired(false);
+        groupEp.setType("string");
+        groupEp.setDisplayOrder(4);
+        configProperties.add(groupEp);
+
+        Property userstoreDomain = new Property();
+        userstoreDomain.setName(SCIMProvisioningConnectorConstants.SCIM_USERSTORE_DOMAIN);
+        userstoreDomain.setDisplayName("User Store Domain");
+        userstoreDomain.setRequired(false);
+        userstoreDomain.setType("string");
+        userstoreDomain.setDisplayOrder(5);
+        configProperties.add(userstoreDomain);
+
+        Property enablePwdProvisioning = new Property();
+        enablePwdProvisioning.setName(SCIMProvisioningConnectorConstants.SCIM_ENABLE_PASSWORD_PROVISIONING);
+        enablePwdProvisioning.setDisplayName("Enable Password Provisioning");
+        enablePwdProvisioning.setRequired(false);
+        enablePwdProvisioning.setDescription("Enable User password provisioning to a SCIM domain");
+        enablePwdProvisioning.setType("boolean");
+        enablePwdProvisioning.setDefaultValue("true");
+        enablePwdProvisioning.setDisplayOrder(6);
+
+        SubProperty defaultPwd = new SubProperty();
+        defaultPwd.setName(SCIMProvisioningConnectorConstants.SCIM_DEFAULT_PASSWORD);
+        defaultPwd.setDisplayName("Default Password");
+        defaultPwd.setRequired(false);
+        defaultPwd.setType("string");
+        enablePwdProvisioning.setSubProperties(new SubProperty[] {defaultPwd});
+        configProperties.add(enablePwdProvisioning);
+
+        return configProperties;
+    }
 }
